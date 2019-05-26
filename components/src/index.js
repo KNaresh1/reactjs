4c83cea942b6ce84bbf8bcbe9e3a35e1d23bc67a
Kakumani <NareshKumar.Kakumani@BNSF.com>
2019-05-25 21:03:20 -0500

    Added reusable components

--- components/src/index.js
+++ components/src/index.js
@@ -1,17 +1,39 @@
 // Import React and ReactDOM libraries
 import React from 'react';
 import ReactDOM from 'react-dom';
+import faker from 'faker';
 import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
 
 // Create a react component
 const App = () => {
 	return ( 
 		<div className="ui container comments">
-			<CommentDetail />
-			<CommentDetail />
-			<CommentDetail />
-			<CommentDetail />
-			<CommentDetail />
+			<ApprovalCard>
+				<CommentDetail 
+					author="Sam" 
+					timeAgo="Today at 04:30 PM" 
+					content="Awesome content" 
+					avatar={faker.image.avatar()}
+				/>
+			</ApprovalCard>
+			<ApprovalCard>
+				<CommentDetail 
+					author="Peter" 
+					timeAgo="Today at 02:10 PM" 
+					content="Nice blog post!" 
+					avatar={faker.image.avatar()} 
+				/>
+			</ApprovalCard>
+			<ApprovalCard>
+				<div>
+					<h4>Warning!</h4>
+					Are you sure you want to do this?
+				</div>
+			</ApprovalCard>
+			<ApprovalCard>
+				<h4>We can pass any content to parent component as it is implemented as reusable component</h4>
+			</ApprovalCard>
 		</div>
 	);
 };
