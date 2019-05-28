9a00ffaa108597917332d05f608e0faa5597cf76
Kakumani <NareshKumar.Kakumani@BNSF.com>
2019-05-27 19:45:18 -0500

    Added video detail component

--- videos/src/components/VideoList.js
+++ videos/src/components/VideoList.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import VideoItem from './VideoItem';
 
-const VideoList = ({ videos }) => {
+const VideoList = ({ videos, onVideoSelect }) => {
 	const videoItems = videos.map(video => {
-		return <VideoItem key={video.id.videoId} />;
+		return <VideoItem 
+					key={video.id.videoId} 
+					video={video} 
+					onVideoSelect={onVideoSelect}
+				/>;
 	});
 
-	return <div>{videoItems}</div>;
+	return <div className="ui relaxed divided list">{videoItems}</div>;
 };
 
 export default VideoList;
